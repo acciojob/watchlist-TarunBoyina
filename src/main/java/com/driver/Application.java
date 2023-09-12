3d67cc6bf1f20b8a79dbf7ddfa91ce1c7d165b3d
TarunBoyina <tarunboyina33@gmail.com>
2023-09-12 11:57:58 +0530

    done

--- src/main/java/com/driver/Application.java
+++ src/main/java/com/driver/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class giApplication {
 
 	public static void main(String[] args) {
 
